ece4ab73026e6c057967e176f333a91f631e7bab
Hotaru(Naokibot) <sagakatsuyuki.555@gmail.com>
2026-08-14 20:10:55 +0900

    Fix Deno linted test import

--- tests/time_test.ts
+++ tests/time_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals, assertThrows } from "https://deno.land/std@0.224.0/assert/mod.ts";
+import { assertEquals, assertThrows } from "@std/assert";
 import { countdownMessage } from "../lib/messages.ts";
 import { daysUntil, jstDateTime, nextJstOccurrence, parseDate, parseTime } from "../lib/time.ts";
 
